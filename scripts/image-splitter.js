f61d3c0dba60bf4a494249bdd3c6669a7187149d
Ryan schmidt <ryanschmidt@Ryans-New-MacBook-Air.local>
2026-07-02 19:37:22 -0500

    Finalize member portal status section and image splitter production behavior

--- scripts/image-splitter.js
+++ scripts/image-splitter.js
@@ -91,15 +91,9 @@
 
   ImageSplitter.prototype.update = function (percent) {
     this.position = percent;
-
-    if (reducedMotion) {
-      this.after.style.clipPath = 'inset(0 0 0 ' + percent + '%)';
-      this.handle.style.transform = 'translateX(-50%)';
-      this.handle.style.left = percent + '%';
-    } else {
-      this.after.style.clipPath = 'inset(0 0 0 ' + percent + '%)';
-      this.handle.style.left = percent + '%';
-    }
+    this.after.style.clipPath = 'inset(0 0 0 ' + percent + '%)';
+    this.handle.style.left = percent + '%';
+    this.handle.style.transform = 'translateX(-50%)';
     this.handle.setAttribute('aria-valuenow', Math.round(percent));
   };
 
